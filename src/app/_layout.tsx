4213a7dcd69cbfefb0d24bd8431e1fcfd22523d0
Lingadurai03 <lingadurai033@gmail.com>
2025-08-26 16:54:35 +0530

    first comit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,6 @@
+import { Stack } from 'expo-router';
+import '../../global.css'; // important for Tailwind to work
+
+export default function RootLayout() {
+  return <Stack />;
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function Home() {
+  return (
+    <View className="bg-background flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold text-purple-700 ">
+        🚀 Nativewind + App Router is working!
+      </Text>
+    </View>
+  );
+}
